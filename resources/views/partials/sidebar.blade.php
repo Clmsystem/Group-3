b2ee00b4dab48925d6c9792f312d20ecf9ed388c
chotiwich <chotiwich6793@gmail.com>
2021-05-12 01:16:14 +0700

    edit tab

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,48 +1,93 @@
 <div class="container-fluid page-body-wrapper">
-  <nav class="sidebar sidebar-offcanvas" id="sidebar">
-    <ul class="nav">
-      <li class="nav-item nav-profile">
-        <a href="#" class="nav-link">
-          <div class="nav-profile-image">
-            <img src="assets/images/faces/face1.jpg" alt="profile">
-            <span class="login-status online"></span>
-            <!--change to offline or busy as needed-->
-          </div>
-          <div class="nav-profile-text d-flex flex-column">
-            <span class="font-weight-bold mb-2">David Grey. H</span>
-            <span class="text-secondary text-small">Project Manager</span>
-          </div>
-          <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">
-          <span class="menu-title">Dashboard</span>
-          <i class="mdi mdi-home menu-icon"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-          <span class="menu-title">ALL Topic</span>
-          <i class="menu-arrow"></i>
-          <i class="mdi mdi-crosshairs-gps menu-icon"></i>
-        </a>
-        <div class="collapse" id="ui-basic">
-          <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Topic 1</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Topic 2</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Topic 3</a></li>
-            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Topic 4</a></li>
-          </ul>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/icons/mdi.html">
-          <span class="menu-title">Member</span>
-          <i class="mdi mdi-contacts menu-icon"></i>
-        </a>
-      </li>
-    </ul>
-  </nav>
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+            <li class="nav-item nav-profile">
+                <a href="#" class="nav-link">
+                    <div class="nav-profile-image">
+                        <img src="assets/images/faces/face1.jpg" alt="profile">
+                        <span class="login-status online"></span>
+                        <!--change to offline or busy as needed-->
+                    </div>
+                    <div class="nav-profile-text d-flex flex-column">
+                        <span class="font-weight-bold mb-2">David Grey. H</span>
+                        <span class="text-secondary text-small">Project Manager</span>
+                    </div>
+                    <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <span class="menu-title">Dashboard</span>
+                    <i class="mdi mdi-home menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false"
+                    aria-controls="ui-basic">
+                    <span class="menu-title">ส่วนที่ 2</span>
+                    <i class="menu-arrow"></i>
+                    <i class="mdi mdi-crosshairs-gps menu-icon"></i>
+                </a>
+                <div class="collapse" id="ui-basic">
+                    <ul class="nav flex-column sub-menu">
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/createPart2">เพิ่มตัวชี้วัด</a></li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/contentPart2">เพิ่มข้อมูลรายเดือน</a></li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/contentPart2Year">เพิ่มข้อมูลรายปี</a></li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/searchPart2">ค้นหาข้อมูลย้อนหลัง</a></li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/confirmPart2">ยืนยันข้อมูลตัวชี้วัดรายเดือน</a></li>
+                        <li class="nav-item"> <a class="nav-link"
+                                href="http://127.0.0.1:8000/confirmPart2Year">ยืนยันข้อมูลตัวชี้วัดรายปี</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/createPart2">
+                    <span class="menu-title">เพิ่มตัวชี้วัด</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/contentPart2">
+                    <span class="menu-title">เพิ่มข้อมูลรายเดือน</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/contentPart2Year">
+                    <span class="menu-title">เพิ่มข้อมูลรายปี</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/searchPart2">
+                    <span class="menu-title">ค้นหาข้อมูลย้อนหลัง</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/confirmPart2">
+                    <span class="menu-title">ยืนยันข้อมูลตัวชี้วัดรายเดือน</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://127.0.0.1:8000/confirmPart2Year">
+                    <span class="menu-title">ยืนยันข้อมูลตัวชี้วัดรายปี</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="pages/icons/mdi.html">
+                    <span class="menu-title">Member</span>
+                    <i class="mdi mdi-contacts menu-icon"></i>
+                </a>
+            </li>
+        </ul>
+    </nav>
 
-  <!-- partial -->
+    <!-- partial -->
